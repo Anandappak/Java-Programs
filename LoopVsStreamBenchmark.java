81e84811821efd2de125d73c8d94195f677b336a
Anand Dev <90673871+Anandappak@users.noreply.github.com>
2025-09-25 13:38:16 +0530

    Create LoopVsStreamBenchmark.java
    LoopVsStreamBenchmark

---
+++ LoopVsStreamBenchmark.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+import java.util.stream.*;
+
+public class LoopVsStreamBenchmark {
+    public static void main(String[] args) {
+        // Prepare data (1 million integers)
+        List<Integer> data = IntStream.rangeClosed(1, 1_000_000)
+                                      .boxed()
+                                      .collect(Collectors.toList());
+        int target = 500_000;
+
+        // ---------- For loop with break ----------
+        long start = System.nanoTime();
+        Integer foundLoop = null;
+        for (Integer num : data) {
+            if (num == target) {
+                foundLoop = num;
+                break;
+            }
+        }
+        long end = System.nanoTime();
+        long loopTime = end - start;
+
+        // ---------- Stream findFirst ----------
+        start = System.nanoTime();
+        Integer foundStream = data.stream()
+                                  .filter(num -> num == target)
+                                  .findFirst()
+                                  .orElse(null);
+        end = System.nanoTime();
+        long streamTime = end - start;
+
+        // ---------- Results ----------
+        System.out.println("For loop found: " + foundLoop + " in " + loopTime / 1_000_000.0 + " ms");
+        System.out.println("Stream found   : " + foundStream + " in " + streamTime / 1_000_000.0 + " ms");
+    }
+}
